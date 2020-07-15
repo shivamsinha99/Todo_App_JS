78fffe10741a30405e2116546977de0c514ccb40
Shivam Sinha <shivam.sinha9966@gmail.com>
2020-07-16 02:44:15 +0530

    added all/todo/completed options

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
+const filterOption = document.querySelector(".filter-todo");
 
 todoButton.addEventListener('click', addTodo);
 todoList.addEventListener('click', deleteCheck);
+filterOption.addEventListener('click', filterTodo);
 
 function addTodo(event){
     event.preventDefault(); // prevents refreshing
@@ -26,7 +28,7 @@ function addTodo(event){
     trashButton.classList.add("trash-btn");
     todoDiv.appendChild(trashButton);
 
-    todoList.appendChild(todoDiv)
+    todoList.appendChild(todoDiv);
 
     todoInput.value = "";
 }
@@ -45,4 +47,30 @@ function deleteCheck(e){
         const todo = item.parentElement;
         todo.classList.toggle("completed")
     }
+}
+
+function filterTodo(e){
+    const todos = todoList.childNodes;
+    // console.log("debug");
+    todos.forEach(function(todo){
+        switch(e.target.value) {
+            case "all":
+                todo.style.display = "flex";
+                break;
+            case "completed":
+                if(todo.classList.contains("completed")) {
+                    todo.style.display = "flex";               
+                } else {
+                    todo.style.display = "none";
+                }
+                break;
+            case "todo":
+                if(!todo.classList.contains("completed")) {
+                    todo.style.display = "flex";               
+                } else {
+                    todo.style.display = "none";
+                }
+                break;
+        }
+    });
 }
